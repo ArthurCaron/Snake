da8c6daabcd21ab89386cee65284df56ae1c5a81
Arthur Caron <22469379+ArthurCaron@users.noreply.github.com>
2025-11-08 15:03:09 +0100

    Default LibGDX project

---
+++ teavm/src/main/kotlin/fr/mwet/snake/teavm/TeaVMBuilder.kt
@@ -0,0 +1,31 @@
+package fr.mwet.snake.teavm
+
+import java.io.File
+import com.github.xpenatan.gdx.backends.teavm.config.AssetFileHandle
+import com.github.xpenatan.gdx.backends.teavm.config.TeaBuildConfiguration
+import com.github.xpenatan.gdx.backends.teavm.config.TeaBuilder
+import com.github.xpenatan.gdx.backends.teavm.config.plugins.TeaReflectionSupplier
+import com.github.xpenatan.gdx.backends.teavm.gen.SkipClass
+import org.teavm.vm.TeaVMOptimizationLevel
+
+/** Builds the TeaVM/HTML application. */
+@SkipClass
+object TeaVMBuilder {
+    @JvmStatic fun main(arguments: Array<String>) {
+        val teaBuildConfiguration = TeaBuildConfiguration().apply {
+            assetsPath.add(AssetFileHandle("../assets"))
+            webappPath = File("build/dist").canonicalPath
+            // Register any extra classpath assets here:
+            // additionalAssetsClasspathFiles += "fr/mwet/snake/asset.extension"
+        }
+
+        // Register any classes or packages that require reflection here:
+        // TeaReflectionSupplier.addReflectionClass("fr.mwet.snake.reflect")
+
+        val tool = TeaBuilder.config(teaBuildConfiguration)
+        tool.mainClass = "fr.mwet.snake.teavm.TeaVMLauncher"
+        tool.optimizationLevel = TeaVMOptimizationLevel.FULL
+        tool.setObfuscated(true)
+        TeaBuilder.build(tool)
+    }
+}
