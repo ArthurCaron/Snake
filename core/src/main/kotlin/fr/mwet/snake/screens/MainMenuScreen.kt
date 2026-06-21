2565f57b87a2157785487aae7dbc3b4484df0332
Arthur Caron <22469379+ArthurCaron@users.noreply.github.com>
2026-06-21 14:17:31 +0200

    Improve main menu

--- core/src/main/kotlin/fr/mwet/snake/screens/MainMenuScreen.kt
+++ core/src/main/kotlin/fr/mwet/snake/screens/MainMenuScreen.kt
@@ -3,9 +3,12 @@ package fr.mwet.snake.screens
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.math.Interpolation
+import com.badlogic.gdx.math.Interpolation.elasticOut
+import com.badlogic.gdx.math.Interpolation.linear
+import com.badlogic.gdx.scenes.scene2d.Action
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.actions.Actions
+import com.badlogic.gdx.scenes.scene2d.actions.Actions.*
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
 import com.badlogic.gdx.utils.Align
@@ -31,106 +34,47 @@ import ktx.scene2d.scene2d
 
 @Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
 class MainMenuScreen(
-    private val textureHandler: TextureHandler,
-    private val menuEventBus: MenuEventBus,
+    textureHandler: TextureHandler,
+    menuEventBus: MenuEventBus,
     private val stage: Stage,
     private val batch: SpriteBatch,
     private val gameViewport: GameViewport,
     private val stageViewport: StageViewport,
     private val gameCamera: OrthographicCamera,
 ) : MenuEventListener, KtxScreen, DisposableRegistry by DisposableContainer() {
-    private var clickedPlayBtn = false
-    val playBtnDrawable = TextureRegionDrawable(textureHandler.playBtn)
-    val playBtnDownDrawable = TextureRegionDrawable(textureHandler.playBtnDown)
-
-    private val gameTitle: Image = scene2d.image(textureHandler.gameTitle) {
-        val defaultRatio = height / width
-        width = gameViewport.screenWidth * 0.70f
-        height = width * defaultRatio
-        setOrigin(Align.center)
-    }
-
-    private val playBtn: Image = scene2d.image(playBtnDrawable) {
-        val defaultRatio = height / width
-        width = gameViewport.screenWidth * 0.95f * 0.5f
-        height = width * defaultRatio
-        setOrigin(Align.center)
-    }.apply {
-        onClick {
-            menuEventBus.emit(MenuEvent.PlayGameClicked)
-        }
-        onEnter {
-            playBtn.addAction(Actions.scaleTo(1.25f, 1.25f, 0.3f, Interpolation.elasticOut))
-        }
-        onExit {
-            if (!clickedPlayBtn) {
-                playBtn.addAction(Actions.scaleTo(1f, 1f, 0.5f, Interpolation.elasticOut))
-            }
-        }
-    }
+    private val backgroundTexture = textureHandler.background
+    private val mainMenuTitle = MainMenuTitle(textureHandler, gameViewport)
+    private val playButton = PlayButton(textureHandler, gameViewport, menuEventBus)
 
     override fun show() {
-        animateGameTitle()
-        animatePlayButton()
         DI.registerGeneralInputProcessor()
+
+        mainMenuTitle.startAnimation()
+        playButton.startAnimation()
     }
 
     override fun hide() {
         DI.unRegisterGeneralInputProcessor()
+
+        mainMenuTitle.resetAnimation()
+        playButton.resetAnimation()
     }
 
     override fun resize(width: Int, height: Int) {
         stage.clear()
+        stage.addActor(mainMenuTitle.actor)
+        stage.addActor(playButton.actor)
 
-        stage.addActor(gameTitle)
-        positionGameTitle()
-
-        stage.addActor(playBtn)
-        positionPlayButton()
-    }
-
-    private fun positionGameTitle() {
-        gameTitle.setPosition(
-            (Gdx.graphics.width - gameTitle.width) * 0.5f,
-            (Gdx.graphics.height - gameTitle.height) * 0.75f
-        )
-    }
-
-    private fun positionPlayButton() {
-        playBtn.setPosition(
-            (Gdx.graphics.width - playBtn.width) * 0.5f,
-            (Gdx.graphics.height - playBtn.height) * 0.25f
-        )
-    }
-
-    private fun animateGameTitle() {
-        gameTitle.setScale(0.001f)
-        gameTitle.addAction(
-            Actions.delay(0.5f, Actions.scaleTo(1f, 1f, 1.25f, Interpolation.elasticOut))
-        )
-    }
-
-    private fun animatePlayButton() {
-        playBtn.setColor(1f, 1f, 1f, 0f)
-        playBtn.addAction(Actions.delay(0.75f, Actions.fadeIn(0.35f)))
-        val moveY = gameTitle.height * 0.1f
-        playBtn.addAction(
-            Actions.forever(
-                Actions.sequence(
-                    Actions.moveBy(0f, moveY, 1f, Interpolation.linear),
-                    Actions.moveBy(0f, -moveY, 1f, Interpolation.linear)
-                )
-            )
-        )
+        mainMenuTitle.resetPosition()
+        playButton.resetPosition()
     }
 
     override fun render(delta: Float) {
         gameViewport.apply(true)
         batch.use(gameCamera) {
-            it.draw(textureHandler.background, 0f, 0f, WORLD_WIDTH, WORLD_HEIGHT)
+            it.draw(backgroundTexture, 0f, 0f, WORLD_WIDTH, WORLD_HEIGHT)
         }
 
-        // Draw stage actors
         stageViewport.apply(true)
         stage.act(delta)
         stage.draw()
@@ -138,17 +82,119 @@ class MainMenuScreen(
 
     override fun onEvent(event: MenuEvent) {
         when (event) {
-            MenuEvent.PlayGameClicked -> playGame()
+            MenuEvent.PlayGameClicked -> {
+                playButton.startGame()
+            }
+        }
+    }
+}
+
+private class MainMenuTitle(
+    textureHandler: TextureHandler,
+    private val gameViewport: GameViewport,
+) {
+    val actor: Image = scene2d.image(textureHandler.gameTitle) {
+        val defaultRatio = height / width
+        width = gameViewport.screenWidth * 0.70f
+        height = width * defaultRatio
+        setOrigin(Align.center)
+    }
+
+    fun resetPosition() {
+        actor.setPosition(
+            (Gdx.graphics.width - actor.width) * 0.5f,
+            (Gdx.graphics.height - actor.height) * 0.75f
+        )
+    }
+
+    fun startAnimation() {
+        actor.setScale(0.001f)
+        actor.addAction(
+            sequence(
+                delay(0.5f),
+                scaleTo(1f, 1f, 1.25f, elasticOut),
+            )
+        )
+    }
+
+    fun resetAnimation() {
+        actor.clearActions()
+    }
+}
+
+private class PlayButton(
+    textureHandler: TextureHandler,
+    private val gameViewport: GameViewport,
+    menuEventBus: MenuEventBus,
+) {
+    private var clicked = false
+    private val up = TextureRegionDrawable(textureHandler.playBtn)
+    private val down = TextureRegionDrawable(textureHandler.playBtnDown)
+
+    private var hoverAction: Action? = null
+    val actor: Image = scene2d.image(up) {
+        val defaultRatio = height / width
+        width = gameViewport.screenWidth * 0.95f * 0.5f
+        height = width * defaultRatio
+        setOrigin(Align.center)
+    }.apply {
+        val replaceHoverAction = { action: Action ->
+            hoverAction?.let { removeAction(it) }
+            hoverAction = action
+            addAction(action)
+        }
+
+        onClick { menuEventBus.emit(MenuEvent.PlayGameClicked) }
+        onEnter { replaceHoverAction(scaleTo(1.25f, 1.25f, 0.3f, elasticOut)) }
+        onExit {
+            if (!clicked) {
+                replaceHoverAction(scaleTo(1f, 1f, 0.5f, elasticOut))
+            }
         }
     }
 
-    fun playGame() {
-        clickedPlayBtn = true
-        playBtn.drawable = playBtnDownDrawable
-        playBtn.addAction(Actions.delay(0.3f, Actions.run {
-            clickedPlayBtn = false
-            playBtn.drawable = playBtnDrawable
-            Game.setScreen<GameScreen>()
-        }))
+    fun startGame() {
+        if (clicked) return
+
+        clicked = true
+        actor.drawable = down
+
+        actor.addAction(
+            sequence(
+                delay(0.3f),
+                Actions.run { Game.setScreen<GameScreen>() },
+            ),
+        )
+    }
+
+    fun resetPosition() {
+        actor.setPosition(
+            (Gdx.graphics.width - actor.width) * 0.5f,
+            (Gdx.graphics.height - actor.height) * 0.25f
+        )
+    }
+
+    fun startAnimation() {
+        actor.setColor(1f, 1f, 1f, 0f)
+        actor.addAction(
+            parallel(
+                delay(0.75f, fadeIn(0.35f)),
+                forever(
+                    sequence(
+                        moveBy(0f, 5f, 1f, linear),
+                        moveBy(0f, -5f, 1f, linear)
+                    )
+                )
+            )
+        )
+    }
+
+    fun resetAnimation() {
+        actor.clearActions()
+        clicked = false
+        hoverAction = null
+        actor.drawable = up
+        actor.setScale(1f)
+        actor.setColor(1f, 1f, 1f, 0f)
     }
 }
